9f63d11c5b1258ac493f0f7777e536a68566f0c4
Ryan Caloras <rcaloras@gmail.com>
2012-10-07 00:43:17 -0400

    Updated Metadata creation

--- src/main/java/com/rcaloras/addon/timestamp/TimestampMetadata.java
+++ src/main/java/com/rcaloras/addon/timestamp/TimestampMetadata.java
@@ -32,6 +32,9 @@ public class TimestampMetadata extends AbstractItdTypeDetailsProvidingMetadataIt
     // Constants
     private static final String PROVIDES_TYPE_STRING = TimestampMetadata.class.getName();
     private static final String PROVIDES_TYPE = MetadataIdentificationUtils.create(PROVIDES_TYPE_STRING);
+    
+    private static final String CREATED_FIELD="created";
+	private static final String UPDATED_FIELD="updated";
 
     public static final String getMetadataIdentiferType() {
         return PROVIDES_TYPE;
@@ -57,69 +60,77 @@ public class TimestampMetadata extends AbstractItdTypeDetailsProvidingMetadataIt
         super(identifier, aspectName, governorPhysicalTypeMetadata);
         Validate.isTrue(isValid(identifier), "Metadata identification string '" + identifier + "' does not appear to be a valid");
 
-        // Adding a new sample field definition
-        builder.addField(getSampleField());
+        builder.addField(getTimestampField(CREATED_FIELD));
+		builder.addField(getTimestampField(UPDATED_FIELD));
             
         // Adding a new sample method definition
-        builder.addMethod(getSampleMethod());
+		builder.addMethod(getTimestampMethod("onCreate",CREATED_FIELD,"javax.persistence.PrePersist"));
+		builder.addMethod(getTimestampMethod("onUpdate",UPDATED_FIELD,"javax.persistence.PreUpdate"));
         
         // Create a representation of the desired output ITD
         itdTypeDetails = builder.build();
     }
     
-    /**
-     * Create metadata for a field definition. 
-     *
-     * @return a FieldMetadata object
-     */
-    private FieldMetadata getSampleField() {
-        // Note private fields are private to the ITD, not the target type, this is undesirable if a dependent method is pushed in to the target type
-        int modifier = 0;
-        
-        // Using the FieldMetadataBuilder to create the field definition. 
-        final FieldMetadataBuilder fieldBuilder = new FieldMetadataBuilder(getId(), // Metadata ID provided by supertype
-            modifier, // Using package protection rather than private
-            new ArrayList<AnnotationMetadataBuilder>(), // No annotations for this field
-            new JavaSymbolName("sampleField"), // Field name
-            JavaType.STRING); // Field type
-        
-        return fieldBuilder.build(); // Build and return a FieldMetadata instance
+    
+    private FieldMetadata getTimestampField(String fieldName){
+    	
+    	List<AnnotationMetadataBuilder> list = new ArrayList<AnnotationMetadataBuilder>();
+		AnnotationMetadataBuilder dateTimeFormat=new AnnotationMetadataBuilder(new JavaType("org.springframework.format.annotation.DateTimeFormat"));
+		dateTimeFormat.addStringAttribute("style", "M-");
+
+		AnnotationMetadataBuilder temporal=new AnnotationMetadataBuilder(new JavaType("javax.persistence.Temporal"));
+		temporal.addEnumAttribute("value", "javax.persistence.TemporalType", "TIMESTAMP");
+
+		list.add(temporal);
+		list.add(dateTimeFormat);
+
+		// Using the FieldMetadataBuilder to create the field definition. 
+		FieldMetadataBuilder fieldBuilder = new FieldMetadataBuilder(getId(), // Metadata ID provided by supertype
+			Modifier.PRIVATE, // Using package protection rather than private
+			list, // No annotations for this field
+			new JavaSymbolName(fieldName), // Field name
+			new JavaType("java.util.Date")); // Field type
+
+		return fieldBuilder.build(); // Build and return a FieldMetadata instance
+    	
     }
     
-    private MethodMetadata getSampleMethod() {
-        // Specify the desired method name
-        JavaSymbolName methodName = new JavaSymbolName("sampleMethod");
-        
-        // Check if a method with the same signature already exists in the target type
-        final MethodMetadata method = methodExists(methodName, new ArrayList<AnnotatedJavaType>());
-        if (method != null) {
-            // If it already exists, just return the method and omit its generation via the ITD
-            return method;
-        }
-        
-        // Define method annotations (none in this case)
-        List<AnnotationMetadataBuilder> annotations = new ArrayList<AnnotationMetadataBuilder>();
-        
-        // Define method throws types (none in this case)
-        List<JavaType> throwsTypes = new ArrayList<JavaType>();
-        
-        // Define method parameter types (none in this case)
-        List<AnnotatedJavaType> parameterTypes = new ArrayList<AnnotatedJavaType>();
-        
-        // Define method parameter names (none in this case)
-        List<JavaSymbolName> parameterNames = new ArrayList<JavaSymbolName>();
-        
-        // Create the method body
-        InvocableMemberBodyBuilder bodyBuilder = new InvocableMemberBodyBuilder();
-        bodyBuilder.appendFormalLine("System.out.println(\"Hello World\");");
-        
-        // Use the MethodMetadataBuilder for easy creation of MethodMetadata
-        MethodMetadataBuilder methodBuilder = new MethodMetadataBuilder(getId(), Modifier.PUBLIC, methodName, JavaType.VOID_PRIMITIVE, parameterTypes, parameterNames, bodyBuilder);
-        methodBuilder.setAnnotations(annotations);
-        methodBuilder.setThrowsTypes(throwsTypes);
-        
-        return methodBuilder.build(); // Build and return a MethodMetadata instance
-    }
+
+    private MethodMetadata getTimestampMethod(String methodNameStr,String field,String methodAnotation) {
+		// Specify the desired method name
+		JavaSymbolName methodName = new JavaSymbolName(methodNameStr);
+
+		// Check if a method with the same signature already exists in the target type
+		MethodMetadata method = methodExists(methodName, new ArrayList<AnnotatedJavaType>());
+		if (method != null) {
+			// If it already exists, just return the method and omit its generation via the ITD
+			return method;
+		}
+
+		AnnotationMetadataBuilder prePersists=new AnnotationMetadataBuilder(new JavaType(methodAnotation));
+		// Define method annotations (none in this case)
+		List<AnnotationMetadataBuilder> annotations = new ArrayList<AnnotationMetadataBuilder>();
+		annotations.add(prePersists);
+
+		// Define method throws types (none in this case)
+		List<JavaType> throwsTypes = new ArrayList<JavaType>();
+
+		// Define method parameter types (none in this case)
+		List<AnnotatedJavaType> parameterTypes = new ArrayList<AnnotatedJavaType>();
+
+		// Define method parameter names (none in this case)
+		List<JavaSymbolName> parameterNames = new ArrayList<JavaSymbolName>();
+
+		// Create the method body
+		InvocableMemberBodyBuilder bodyBuilder = new InvocableMemberBodyBuilder();
+		bodyBuilder.appendFormalLine("this."+field+"=new java.util.Date();");
+
+		// Use the MethodMetadataBuilder for easy creation of MethodMetadata
+		MethodMetadataBuilder methodBuilder = new MethodMetadataBuilder(getId(), Modifier.PUBLIC, methodName, JavaType.VOID_PRIMITIVE, parameterTypes, parameterNames, bodyBuilder);
+		methodBuilder.setAnnotations(annotations);
+		methodBuilder.setThrowsTypes(throwsTypes);
+		return methodBuilder.build(); // Build and return a MethodMetadata instance
+	}
         
     private MethodMetadata methodExists(JavaSymbolName methodName, List<AnnotatedJavaType> paramTypes) {
         // We have no access to method parameter information, so we scan by name alone and treat any match as authoritative
@@ -132,8 +143,6 @@ public class TimestampMetadata extends AbstractItdTypeDetailsProvidingMetadataIt
         }
         return null;
     }
-    
-    // Typically, no changes are required beyond this point
     
     public String toString() {
         final ToStringBuilder builder = new ToStringBuilder(this);
